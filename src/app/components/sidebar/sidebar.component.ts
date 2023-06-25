cf00af3ec1eab93b3aec4d755304a4df58365437
Da-achraf <achraf.boukir01@gmail.com>
2023-06-25 03:22:18 +0100

    sidebar navigation

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,21 +8,81 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class SidebarComponent {
 
-  collapse : boolean = true;
+
+  sidebarItems = [
+    { title: 'Dashbord', icon: 'fa-house', link: '/client/list', isActiveLight: true, isActiveDark: false },
+    { title: 'Clients', icon: 'fa-hospital-user', link: '/client/list', isActiveLight: false, isActiveDark: false },
+    { title: 'Heartbeats', icon: 'fa-chart-line', link: '/client/heartbeat', isActiveLight: false, isActiveDark: false },
+  ];
+
+  selectedItem: any;
+
+  collapse : boolean = false;
+
+  @Output() darkModeToggled: EventEmitter<boolean> = new EventEmitter<boolean>();
+  darkModeEnabled = false;
+
+  constructor(private router: Router, private activatedRoute:ActivatedRoute){}
+
+  ngOnInit(): void {
+    const storedArray = localStorage.getItem('sideBarItems');
+    const storedVar = localStorage.getItem('darkModeEnabled');
+    console.log(storedArray);
+
+    if (storedArray && storedVar) {
+      console.log('hey');
+
+      this.sidebarItems = JSON.parse(storedArray);
+      this.darkModeEnabled = JSON.parse(storedVar);
+      console.log('Array retrieved:', this.sidebarItems);
+
+      this.selectedItem = this.sidebarItems.find(item => {
+        return (item.isActiveDark || item.isActiveLight);
+      })
+
+    } else {
+      console.log('No array found in local storage.');
+    }
+  }
 
   hide() : void {
     this.collapse = this.collapse;
   }
 
 
-  @Output() darkModeToggled: EventEmitter<boolean> = new EventEmitter<boolean>();
-  darkModeEnabled = false;
-
   toggleDarkMode() {
+    let inedx = this.sidebarItems.findIndex(item => {
+      return item.title == this.selectedItem.title;
+    });
+
+    this.sidebarItems[inedx].isActiveDark = !this.sidebarItems[inedx].isActiveDark;
+    this.sidebarItems[inedx].isActiveLight = !this.sidebarItems[inedx].isActiveLight;
+
     this.darkModeEnabled = !this.darkModeEnabled;
     this.darkModeToggled.emit(this.darkModeEnabled);
   }
 
 
+  setActiveItem(item: any): void {
+    this.selectedItem = item;
+    let inedx = this.sidebarItems.findIndex(item => {
+      return item.title == this.selectedItem.title;
+    });
+
+    this.sidebarItems[inedx].isActiveDark = !this.sidebarItems[inedx].isActiveDark;
+    this.sidebarItems[inedx].isActiveLight = !this.sidebarItems[inedx].isActiveLight;
+    this.sidebarItems.forEach((sidebarItem) => {
+      sidebarItem.isActiveLight = (sidebarItem === item && !this.darkModeEnabled);
+      sidebarItem.isActiveDark = (sidebarItem === item && this.darkModeEnabled);
+    });
+
+    localStorage.setItem('sideBarItems', JSON.stringify(this.sidebarItems));
+    localStorage.setItem('darkModeEnabled', JSON.stringify(this.darkModeEnabled));
+
+  }
+
+  ngOnDestroy(): void {
+    console.log('destroy');
+  }
 
 }
